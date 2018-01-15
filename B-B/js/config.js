f0d4e3c35a96d8a122fe3b95a3c73ff8b8781a93
learnerdr <996060779@qq.com>
2018-01-15 19:48:19 +0800

    B-B

---
+++ B-B/js/config.js
@@ -0,0 +1,26 @@
+/**
+ * incView:打开带原生导航条的页面(无返回按钮)
+ * usuView:打开新页面
+ * NativeView：打开带原生导航条的页面(有返回按钮)
+ * url：打开页面相对路径
+ * id：页面id
+ * title：导航条上显示的名称
+ */
+var openView = {
+	usuView: function(url, id, extras,bounce) {
+		var extras = extras || '';
+		var bounce = bounce ||'none';
+		mui.openWindow({
+			url: url,
+			id: id,
+			waiting: {
+				autoShow: false,
+			},
+			extras: extras,
+			styles:{
+				bounce:bounce
+			}
+		})
+	}
+}
+
